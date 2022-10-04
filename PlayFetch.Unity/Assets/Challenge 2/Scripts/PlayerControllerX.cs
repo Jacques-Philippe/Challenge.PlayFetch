f61dec73bae3133ed15f29c62182ab5b81334495
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-10-04 17:51:06 -0400

    add delay to dog spawn

--- PlayFetch.Unity/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ PlayFetch.Unity/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,31 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    /// <summary>
+    /// The delay between dog spawns
+    /// </summary>
+    private float delay = 3.0f;
+
+    private bool isLocked = false;
+
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            StartCoroutine(SpawnDog());
+        }
+    }
+
+    IEnumerator SpawnDog()
+    {
+        if (!isLocked)
+        {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            isLocked = true;
+            yield return new WaitForSeconds(delay);
+            isLocked = false;
         }
     }
 }
