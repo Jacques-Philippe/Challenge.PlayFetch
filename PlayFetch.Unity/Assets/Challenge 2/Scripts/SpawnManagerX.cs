ecd2fd3b5b4c1e5518da8b36b3796baa58b82bc3
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-10-04 17:58:27 -0400

    Add random delay to ball spawn

--- PlayFetch.Unity/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ PlayFetch.Unity/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,20 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        StartCoroutine(SpawnWithRandomDelay());
+    }
+
+    IEnumerator SpawnWithRandomDelay()
+    {
+        float spawnInterval = Random.Range(3, 5);
+        yield return new WaitForSeconds(spawnInterval);
+        SpawnRandomBall();
+        yield return SpawnWithRandomDelay();
     }
 
     // Spawn random ball at random x position at top of play area
