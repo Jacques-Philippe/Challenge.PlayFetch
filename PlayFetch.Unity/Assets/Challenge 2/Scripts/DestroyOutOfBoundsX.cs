7f31ff72d66fe39f38fe0d99399eeb3a9e28c881
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-10-04 17:49:21 -0400

    Fix bad logic for destroy out of bounds x

--- PlayFetch.Unity/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ PlayFetch.Unity/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
+    private float leftLimit = -50;
     private float bottomLimit = -5;
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        if (transform.position.x < leftLimit)
         {
+            Debug.Log($"X position is less than limit - destroying {this.gameObject.name}");
             Destroy(gameObject);
         } 
         // Destroy balls if y position is less than bottomLimit
         else if (transform.position.z < bottomLimit)
         {
+            Debug.Log($"Z position is less than limit - destroying {this.gameObject.name}");
             Destroy(gameObject);
         }
 
